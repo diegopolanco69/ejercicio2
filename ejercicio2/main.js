de5fb3d1bce18e6e049ec56ae9ec7a4621c4106d
diegopolanco69 <98666928+diegopolanco69@users.noreply.github.com>
2023-03-19 01:44:03 -0400

    Add files via upload

---
+++ ejercicio2/main.js
@@ -0,0 +1,7 @@
+// Crear un array con 3 valores numericos y sumar todos esos valores accedendolos mediante las posiciones.
+
+var valores = [18,20,24];
+
+var suma = valores[0] + valores[1] + valores[2];
+
+console.log(suma);
